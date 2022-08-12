e152e7bae09bb50dddfebc4c9edb767d910d4076
Your Name <rami@shafikov.ca>
2022-08-12 04:47:59 -0400

    Create basic style and layout, js operator functions

---
+++ script.js
@@ -0,0 +1,27 @@
+function add (a, b) {
+    return a + b;
+}
+
+function substract (a, b) {
+    return a - b;
+}
+
+function  multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
+
+function operate (operation, a, b) {
+    if (operation == 'add') {
+       return add(a, b);
+    } else if (operation == 'substract') {
+       return substract(a, b);
+    } else if (operation == 'multiply') {
+        return multiply(a, b);
+    } else if (operation == 'divide') {
+        return divide(a, b);
+    }
+}
